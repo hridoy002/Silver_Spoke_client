8f646113f640dcd721a3b7d0dbcfdf8453d0a4a1
hridoy002 <hridoyalhasan2@gmail.com>
2022-05-24 20:18:46 +0600

    add footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import { Routes,Route } from "react-router-dom";
-import Home from './pages/Home/Home'
+import Home from "./Pages/Home/Home";
+import Footer from "./Pages/shared/Footer";
+import Header from "./Pages/shared/Header";
+
 function App() {
   return (
+    
     <div className="App">
+      <Header />
        <Routes>
           <Route path="/" element={<Home/>} />
        </Routes>
+       <Footer />
     </div>
   );
 }
