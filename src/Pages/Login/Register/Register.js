3526cf308e60fb5b3a1b4897040eca7f9a101dce
hridoy002 <hridoyalhasan2@gmail.com>
2022-05-25 12:53:42 +0600

    register

--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -1,10 +1,132 @@
 import React from 'react';
-
+import { Link,useNavigate } from 'react-router-dom';
+import { useForm } from "react-hook-form";
+import { useCreateUserWithEmailAndPassword, useSignInWithGoogle } from 'react-firebase-hooks/auth'
+import auth from '../../../firebase.init';
+import Loading from '../../shared/Loading';
 const Register = () => {
+    const navigate = useNavigate()
+    const { register, formState: { errors }, handleSubmit } = useForm();
+    const [
+        createUserWithEmailAndPassword,
+        user,
+        loading,
+        error,
+    ] = useCreateUserWithEmailAndPassword(auth);
+
+    if(user){
+        navigate('/');
+    }
+    if(loading){
+        return <Loading />
+    }
+
+    // error 
+    let errorMessage;
+    if (error ) {
+        errorMessage = <p>{error?.message }</p>
+    }
+    // handle form submit 
+    const onSubmit = async data => {
+
+        await createUserWithEmailAndPassword(data.email, data.password);
+        // await updateProfile({ displayName: data.name });
+        console.log()
+        
+    };
     return (
-        <div>
-            register
-        </div>
+        <div className='h-screen flex justify-center items-center'>
+            <div className="card w-96 bg-base-100 shadow-xl flex">
+                <div className="card-body">
+                    <h2 className="text-center text-2xl font-bold">Sign Up</h2>
+
+
+                    {/********** log in form  ***************/}
+                    <form onSubmit={handleSubmit(onSubmit)}>
+
+
+                        {/****************** input user name  ******************/}
+                        <div className="form-control w-full max-w-xs">
+                            <label className="label">
+                                <span className="label-text">Name</span>
+                            </label>
+                            <input
+                                type="text"
+                                placeholder="Your Name"
+                                className="input input-bordered w-full max-w-xs"
+                                {...register("name", {
+                                    required: {
+                                        value: true,
+                                        message: 'Name is Required'
+                                    }
+                                })}
+                            />
+                            <label className="label">
+                                {errors.name?.type === 'required' && <span className="label-text-alt text-red-500">{errors.name.message}</span>}
+                            </label>
+                        </div>
+
+                        {/*********************** email field ********************* */}
+                        <label className="label">
+                            <span className="label-text">Email</span>
+                        </label>
+                        <input
+                            type="email"
+                            placeholder="Your Email"
+                            className="input input-bordered w-full max-w-xs"
+                            {...register("email", {
+                                required: {
+                                    value: true,
+                                    message: 'Email is Required'
+                                },
+                                pattern: {
+                                    value: /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/,
+                                    message: 'Provide a valid Email'
+                                }
+                            })}
+                        />
+                        <label className="label">
+                            {errors.email?.type === 'required' && <span className="label-text-alt text-red-500">{errors.email.message}</span>}
+                            {errors.email?.type === 'pattern' && <span className="label-text-alt text-red-500">{errors.email.message}</span>}
+                        </label>
+
+                        {/*********************** Password field ********************* */}
+                        <div className="form-control w-full max-w-xs">
+                            <label className="label">
+                                <span className="label-text">Password</span>
+                            </label>
+                            <input
+                                type="password"
+                                placeholder="Password"
+                                className="input input-bordered w-full max-w-xs"
+                                {...register("password", {
+                                    required: {
+                                        value: true,
+                                        message: 'Password is Required'
+                                    },
+                                    minLength: {
+                                        value: 6,
+                                        message: 'Must be 6 characters or longer'
+                                    }
+                                })}
+                            />
+                            <label className="label">
+                                {errors.password?.type === 'required' && <span className="label-text-alt text-red-500">{errors.password.message}</span>}
+                                {errors.password?.type === 'minLength' && <span className="label-text-alt text-red-500">{errors.password.message}</span>}
+                            </label>
+                        </div>
+
+                        {errorMessage}
+                        <input className='btn w-full max-w-xs text-white' type="submit" value="Sign Up" />
+                    </form>
+
+
+                    <p><small> Already have an account? <Link to="/login" className='text-secondary'> Please Login</Link></small></p>
+
+                    
+                </div>
+            </div >
+        </div >
     );
 };
 
