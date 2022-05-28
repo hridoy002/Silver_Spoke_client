e08553f4a7dafe1a20757d7307564d74cdfa0bda
hridoy002 <hridoyalhasan2@gmail.com>
2022-05-28 12:19:45 +0600

    my orders

--- src/Pages/DashBoard/MyOrder.js
+++ src/Pages/DashBoard/MyOrder.js
@@ -1,9 +1,49 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
 
 const MyOrder = () => {
+    const [user] = useAuthState(auth);
+    console.log(user.email)
+    const [orders, setOrders] = useState([]);
+    useEffect(() => {
+        fetch(`http://localhost:5000/order?email=${user.email}`)
+            .then(res => res.json())
+            .then(data => setOrders(data))
+    }, [user])
     return (
         <div>
-            myorder
+            <div className="overflow-x-auto my-20">
+                <table className="table w-full">
+                   
+                    <thead>
+                        <tr>
+                            <th>Index</th>
+                            <th>Product Name</th>
+                            <th>Quantity</th>
+                            <th>Address</th>
+                            <th>Phone</th>
+                            <th>Payment</th>
+                            <th>Canceling</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            orders.map((order,index) =><tr>
+                                <th>{index+1}</th>
+                                <td>{order.product}</td>
+                                <td>{order.quantity}</td>
+                                <td>{order.address}</td>
+                                <td>{order.phone}</td>
+                                <td><button className='btn btn-sm btn-success'>Pay</button></td>
+                                <td><button className='btn btn-sm btn-error'>Cancel</button></td>
+                            </tr> )
+                        }
+                        
+                       
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
